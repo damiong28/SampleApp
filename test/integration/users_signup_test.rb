4df795c96e1f0e57fb9e42a2e6d94d1ad0615c6b
Necrostic <damiong28@gmail.com>
2016-04-27 13:24:01 +0000

    Use SSL and the Puma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,6 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password:             "password",
                                             password_confirmation: "password" }
     end
-                                          assert_template 'users/new'
+                                          assert_template 'users/show'
   end
 end
